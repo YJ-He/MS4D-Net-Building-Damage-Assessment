20cee43f27f50c3082982627c7633d1c8cf98aa1
Yongjun He <95644882+YJ-He@users.noreply.github.com>
2024-01-11 22:33:53 -0500

    Update

--- data/dataset_list.py
+++ data/dataset_list.py
@@ -9,7 +9,6 @@ from albumentations.pytorch import ToTensorV2
 
 
 '''for 3 + 3 bands input data(pre+post)'''
-# whu
 rgb_mean = (0.485, 0.456, 0.406, 0.485, 0.456, 0.406)
 rgb_std = (0.229, 0.224, 0.225, 0.229, 0.224, 0.225)
 
